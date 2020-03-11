255b045608dfe9486516fbd3497804f79dc057c3
UI-Developers <info@thesuccesscreed.co.uk>
2020-03-11 21:57:38 +0000

    Finalized Project with Lazy Loading features

--- js/App.js
+++ js/App.js
@@ -1,6 +1,8 @@
 const modal = document.querySelector('#my-modal');
 const modalBtn = document.querySelector('#modal-btn');
 const closeBtn = document.querySelector('.close');
+const skillsToReveal = document.querySelectorAll('.card__skill');
+const workToReveal = document.querySelectorAll('.card');
 
 // Events
 modalBtn.addEventListener('click', openModal);
@@ -23,3 +25,45 @@ function outsideClick(e) {
     modal.style.display = 'none';
   }
 }
+
+// Listen for scroll event
+function events() {
+	window.addEventListener('scroll', () => {
+		skillsToReveal.forEach(el => {
+			calculateIfScrolledTo(el);
+		});
+		workToReveal.forEach(el => {
+			calculateIfScrolledTo(el);
+		});
+	});
+}
+
+events();
+
+function calculateIfScrolledTo(el) {
+	let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100;
+	if (scrollPercent < 90) {
+		el.classList.add('reveal-item--is-visible');
+	}
+}
+
+// Hide Skills and Work elements
+function hideInitially() {
+	skillsToReveal.forEach(el => el.classList.add('reveal-item'));
+	workToReveal.forEach(el => el.classList.add('reveal-item'));
+}
+
+hideInitially();
+
+//jQuery Smooth Scroll
+$('.main-nav a').on('click', function(e) {
+	if (this.hash !== '') {
+		e.preventDefault();
+		const hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top - 70
+		},
+		800
+		);
+	}
+});
